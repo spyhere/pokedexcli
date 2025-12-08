82f469eb0c24d20e21aea60925483d8b5acfea98
spyhere <gekav3@mail.ru>
2025-12-08 21:45:01 +0200

    Move commands type and factory function to separate file

---
+++ commands.go
@@ -0,0 +1,32 @@
+package main
+
+type cliCommand struct {
+	name        string
+	description string
+	cb          func(*Config) error
+}
+
+func GetCommands() map[string]cliCommand {
+	return map[string]cliCommand{
+		"exit": {
+			name:        "exit",
+			description: "Exit the Pokedex",
+			cb:          commandExit,
+		},
+		"help": {
+			name:        "help",
+			description: "Read help of Pokedex",
+			cb:          commandHelp,
+		},
+		"map": {
+			name:        "map",
+			description: "Show next 20 locations",
+			cb:          commandMap,
+		},
+		"mapb": {
+			name:        "mapb",
+			description: "Show previous 20 locations",
+			cb:          commandMapb,
+		},
+	}
+}
--- repl.go
+++ repl.go
@@ -11,37 +11,6 @@ type Config struct {
 	Previous string
 }
 
-type cliCommand struct {
-	name        string
-	description string
-	cb          func(*Config) error
-}
-
-func GetCommands() map[string]cliCommand {
-	return map[string]cliCommand{
-		"exit": {
-			name:        "exit",
-			description: "Exit the Pokedex",
-			cb:          commandExit,
-		},
-		"help": {
-			name:        "help",
-			description: "Read help of Pokedex",
-			cb:          commandHelp,
-		},
-		"map": {
-			name:        "map",
-			description: "Show next 20 locations",
-			cb:          commandMap,
-		},
-		"mapb": {
-			name:        "mapb",
-			description: "Show previous 20 locations",
-			cb:          commandMapb,
-		},
-	}
-}
-
 func CleanInput(text string) []string {
 	return strings.Split(
 		strings.ToLower(strings.TrimSpace(text)),
