c2a53063f077f44bc23fbe40febe0f2e30c4efaf
spyhere <gekav3@mail.ru>
2025-12-08 19:19:44 +0200

    Implement `help` and `exit` commands

--- main.go
+++ main.go
@@ -1,7 +1,27 @@
 package main
 
-import "fmt"
+import (
+	"bufio"
+	"fmt"
+	"log"
+	"os"
+)
 
 func main() {
-	fmt.Println("Hello, World!")
+	scanner := bufio.NewScanner(os.Stdin)
+	commands := GetCommands()
+	for {
+		fmt.Print("Pokedex > ")
+		ok := scanner.Scan()
+		if !ok {
+			log.Fatal("failed reading the text")
+		}
+		first_word := CleanInput(scanner.Text())[0]
+		command, ok := commands[first_word]
+		if !ok {
+			commands["help"].cb()
+			continue
+		}
+		command.cb()
+	}
 }
--- repl.go
+++ repl.go
@@ -1,10 +1,53 @@
 package main
 
-import "strings"
+import (
+	"fmt"
+	"os"
+	"strings"
+)
+
+type cliCommand struct {
+	name        string
+	description string
+	cb          func() error
+}
+
+func GetCommands() map[string]cliCommand {
+	return map[string]cliCommand{
+		"exit": {
+			name:        "exit",
+			description: "Exit the Pokedex",
+			cb:          commandExit,
+		},
+		"help": {
+			name:        "help",
+			description: "Read help of Pokedex",
+			cb:          commandHelp,
+		},
+	}
+}
 
 func CleanInput(text string) []string {
 	return strings.Split(
 		strings.ToLower(strings.TrimSpace(text)),
 		" ",
 	)
+}
+
+func commandExit() error {
+	fmt.Println("Closing the Pokedex... Goodbye!")
+	os.Exit(0)
+	return nil
+}
+
+func commandHelp() error {
+	commandDescription := ""
+	for _, it := range GetCommands() {
+		commandDescription += fmt.Sprintf("%s: %s\n", it.name, it.description)
+	}
+	fmt.Printf(`Welcome to the Pokedex!
+Usage:
+
+%s`, commandDescription)
+	return nil
 }
